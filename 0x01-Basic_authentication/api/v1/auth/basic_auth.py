24735a88314b8389a1ad00e7cc81d47a5a494583
omwami <ianalpha@protonmail.com>
2024-02-16 17:28:13 +0300

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """ Module with BasicAUth class"""
 from api.v1.auth.auth import Auth
+import base64
+from flask import request
 
 
 class BasicAuth(Auth):
     """ BasicAuth class """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """ returns base64 part of auth header """
+        header = authorization_header
+        if not header:
+            return None
+        if not isinstance(header, str):
+            return None
+        if not header.startswith('Basic '):
+            return None
+        return header[len('Basic '):]
