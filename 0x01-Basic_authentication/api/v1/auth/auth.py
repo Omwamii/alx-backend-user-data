31268fe87428c74603fe749bb4576d736f8956f2
omwami <ianalpha@protonmail.com>
2024-02-16 13:14:35 +0300

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+from flask import request
+from Typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication API management app
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns False """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ returns None """
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None """
+        return request
