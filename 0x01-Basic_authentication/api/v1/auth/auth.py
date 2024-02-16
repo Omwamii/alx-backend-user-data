d5abe90681b068c3fb01c71e05c9211d2f2ac42f
omwami <ianalpha@protonmail.com>
2024-02-16 13:46:10 +0300

    Define which routes don't require authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,15 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        # add tolerance for routes not ending with '/'
+        if not path.endswith('/'):
+            path += '/'
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
