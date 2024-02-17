70ffca617107e20fe6df679b8cccdebca8224e86
omwami <ianalpha@protonmail.com>
2024-02-17 19:09:15 +0300

    Regexing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Module with 'filter_datum' method """
+import re
+from typing import List
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ uses regex to replace occurences of strings to obfuscate
+    Args:
+        fields: list of strings indicating fields to be obfuscated
+        redaction: what field will be obfuscated to 
+        message: log line to obfuscate
+        separator: character separating the fields
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separation, message)
+    return message
