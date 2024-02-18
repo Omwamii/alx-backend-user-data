d5d2e7ce19dd2c791b4952d5191b861c910a910e
omwami <ianalpha@protonmail.com>
2024-02-18 15:44:53 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module with SessionAUth class """
+from api.v1.auth.auth import Auth
+from flask import request
+# from models.user import User
+# from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    """ Session Authentication class """
+    pass
