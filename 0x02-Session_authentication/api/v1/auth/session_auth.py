d589aef8df50d53b100a20c059fe471465e87bb5
omwami <ianalpha@protonmail.com>
2024-02-18 16:09:26 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,11 @@ class SessionAuth(Auth):
         sesh_id = uuid.uuid4()
         self.user_id_by_session_id[sesh_id] = user_id
         return sesh_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a session ID """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
