d5e15c1ae29bb8e8a8286e85b6da6dafd0c7ca34
omwami <ianalpha@protonmail.com>
2024-02-22 16:03:10 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,14 @@ class Auth:
         else:
             # user already exists with the provided email
             raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Checks if password is valid for user's email (login)
+        """
+        try:
+            user = self.__db.find_user_by(email=email)
+        except Exception:
+            return False  # user doesn't exist
+        else:
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
