31e1909d3ab257f694d8e9e20f084d673913f635
omwami <ianalpha@protonmail.com>
2024-02-22 10:24:42 +0300

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" module with SQLAlchemy model 'user' """
+from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ Class mapping for user model """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=True)
+    reset_token = Column(VARCHAR(250), nullable=True)
