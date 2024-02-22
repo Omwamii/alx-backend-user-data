7844ab9edadfea512343fec33f5053792749f1ef
omwami <ianalpha@protonmail.com>
2024-02-22 23:41:14 +0300

    find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,16 +26,16 @@ class Auth:
 
     def __init__(self):
         """ Init variables """
-        self.__db = DB()
+        self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
         """ Register a user """
         try:
-            self.__db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
         except NoResultFound:
             hashed_pw = _hash_password(password)
-            new_user = self.__db.add_user(email, hashed_pw)
-            self.__db._session.commit()
+            new_user = self._db.add_user(email, hashed_pw)
+            self._db._session.commit()
             return new_user
         else:
             # user already exists with the provided email
@@ -45,7 +45,7 @@ class Auth:
         """ Checks if password is valid for user's email (login)
         """
         try:
-            user = self.__db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except Exception:
             return False  # user doesn't exist
         else:
@@ -55,12 +55,12 @@ class Auth:
     def create_session(self, email: str) -> str:
         """ finds user with email & returns the session ID as string """
         try:
-            user = self.__db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except Exception:
             return None
         else:
             sesh_id = _generate_uuid()
-            self.__db.update_user(user.id, session_id=sesh_id)
+            self._db.update_user(user.id, session_id=sesh_id)
             return sesh_id
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
@@ -68,8 +68,7 @@ class Auth:
         if not session_id:
             return None
         try:
-            user = self.__db._session.query(User).filter(
-                    session_id=session_id).first()
+            user = self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
         else:
