e42189a0699b4c7c7c3df7a7677ab5a69550e721
omwami <ianalpha@protonmail.com>
2024-02-23 11:20:08 +0300

    generate password reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,7 +62,7 @@ def profile() -> str:
     return jsonify({"email": f"{user.email}"}), 200
 
 
-@app.route("/reset_password", strict_slashes=False)
+@app.route("/reset_password", methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
     """ Generate a password reset token for user with email
     """
